510c95704495870d72bd292e5afdc3b127e111b4
PRINCE BHADANIA <princebhadania007@gmail.com>
2026-03-30 14:43:15 +0530

    Day-30

---
+++ 2264_Largest_Same_Digit_Number_String.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string ans = "";
+
+        for (int i = 1; i < num.size() - 1; i++) {
+            if (num[i] == num[i - 1] && num[i] == num[i + 1]) {
+                string temp = num.substr(i - 1, 3);
+                if (temp > ans) {
+                    ans = temp;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
